42c2e6b096e5bcd2678db2a1c48ccd269c21817f
wzseba <wzseba@gmail.com>
2024-09-29 19:57:15 -0300

    refactor: en desarrollo, ver modificadores de acceso

--- src/game/of/poliform/Soldado.java
+++ src/game/of/poliform/Soldado.java
@@ -1,6 +1,6 @@
 package game.of.poliform;
 
-public class Soldado extends Unidad {
+public class Soldado extends Unidad implements TomadorDeAgua {
 	/*
 	 * Los soldados pueden atacar cuerpo a cuerpo a otras unidades si tienen
 	 * suficiente energía. Cada ataque les consume 10 puntos de energía, y comienzan
@@ -10,15 +10,31 @@ public class Soldado extends Unidad {
 
 	private int energia;
 
-	public Soldado(int salud, int daño) {
-		super(salud, daño);
+	public Soldado(int posicion) {
+		super(200, 10, posicion);
 		this.energia = 100;
 	}
 
 	@Override
-	public void atacar() {
-		// TODO Auto-generated method stub
-
+	public boolean puedeAtacar(Unidad oponente) {
+		return (this != oponente && !this.estaMuerto() && !oponente.estaMuerto() && this.energia >= 10
+				&& estaDentroDelAlcance(oponente, 0, 0));
 	}
 
+	@Override
+	public void atacar(Unidad oponente) {
+		if (this.puedeAtacar(oponente)) {
+			this.energia -= 10;
+			infringirDanio(oponente);
+		}
+	}
+
+	protected int getEnergia() {
+		return energia;
+	}
+
+	@Override
+	public void beberAgua() {
+		this.energia = 100;
+	}
 }
