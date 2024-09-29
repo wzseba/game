6563640e3e3595a13d030acbc7dc8652b4336fff
wzseba <wzseba@gmail.com>
2024-09-29 19:46:27 -0300

    add: desarrollo de logica y metodos en proceso

--- src/game/of/poliform/Unidad.java
+++ src/game/of/poliform/Unidad.java
@@ -3,14 +3,54 @@ package game.of.poliform;
 public abstract class Unidad {
 
 	private int salud;
-	private int daño;
+	private int danio;
+	public int posicion;
 
-	public Unidad(int salud, int daño) {
-
+	public Unidad(int salud, int danio, int posicion) {
 		this.salud = salud;
-		this.daño = daño;
+		this.danio = danio;
+		this.posicion = posicion;
 	}
 
-	public abstract void atacar();
+	public abstract boolean puedeAtacar(Unidad oponente);
+
+	public abstract void atacar(Unidad oponente);
+
+	protected void infringirDanio(Unidad oponente) {
+		oponente.setSalud(oponente.getSalud() - this.getDanio());
+	}
+
+	public boolean estaDentroDelAlcance(Unidad oponente, int desde, int hasta) {
+		return Math.abs(this.getPosicion() - oponente.getPosicion()) >= desde
+				&& Math.abs(this.getPosicion() - oponente.getPosicion()) <= hasta;
+	}
+
+	public boolean estaMuerto() {
+		return (this.getSalud() <= 0);
+	}
+
+	public int getSalud() {
+		return salud;
+	}
+
+	public void setSalud(int salud) {
+		this.salud = salud;
+	}
+
+	public int getDanio() {
+		return danio;
+	}
+
+	public void setDanio(int danio) {
+		this.danio = danio;
+	}
+
+	public void setPosicion(int posicion) {
+		this.posicion = posicion;
+	}
+
+	public int getPosicion() {
+		return posicion;
+	}
 
 }
