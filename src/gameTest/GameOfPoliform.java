735034c991c50fc2fa4c6f60734c4d0b1cfdd167
wzseba <wzseba@gmail.com>
2024-09-30 23:15:22 -0300

    add: estructura basica test unitario

---
+++ src/gameTest/GameOfPoliform.java
@@ -0,0 +1,14 @@
+package gameTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GameOfPoliform {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
