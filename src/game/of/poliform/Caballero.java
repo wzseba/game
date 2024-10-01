f2d1f9894e112f06c5563bc8e6776e16e9c143be
wzseba <wzseba@gmail.com>
2024-09-30 23:10:33 -0300

    refactor: en revision

--- src/game/of/poliform/Caballero.java
+++ src/game/of/poliform/Caballero.java
@@ -1,16 +1,36 @@
 package game.of.poliform;
 
 public class Caballero extends Unidad {
+	/*
+	 * Los caballeros pueden atacar a una distancia de 1 a 2, siempre que su caballo
+	 * no se haya puesto rebelde. Infringe un daño de 50 puntos y comienza con 200
+	 * de salud.
+	 */
 
-	public Caballero(int salud, int daño) {
-		super(salud, daño);
-		// TODO Auto-generated constructor stub
+	private Caballo caballo;
+
+	public Caballero(int posicion) {
+		super(200, 50, posicion);
+//		this.caballo = new Caballo();
 	}
 
 	@Override
-	public void atacar() {
-		// TODO Auto-generated method stub
+	public boolean puedeAtacar(Unidad oponente) {
+		return (this != oponente && !this.estaMuerto() && !oponente.estaMuerto() && estaDentroDelAlcance(oponente, 1, 2)
+				&& !caballo.estaRebelde());
+	}
+
+	@Override
+	public void atacar(Unidad oponente) {
+		if (puedeAtacar(oponente)) {
+			caballo.setContadorAtaque();
+			infringirDanio(oponente);
+		}
 
 	}
+
+//	public Caballo getCaballo() {
+//		return caballo;
+//	}
 
 }
