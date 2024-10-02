eb14235f45752a20d02df7aa1f77c55a037c511b
wzseba <wzseba@gmail.com>
2024-10-01 23:13:03 -0300

    add: test crear una unidad

--- src/gameTest/GameOfPoliform.java
+++ src/gameTest/GameOfPoliform.java
@@ -1,14 +1,19 @@
 package gameTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+
+import game.of.poliform.Soldado;
+import game.of.poliform.Unidad;
 
 class GameOfPoliform {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testCrearSoldado() {
+		Unidad soldado = new Soldado(2);
+
+		assertTrue(soldado instanceof Unidad, "La instancia no es de la clase Unidad");
 	}
 
 }
